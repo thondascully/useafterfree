ca8adfa4405056ee98d7a66354156f21242363f9
thondascully <thondascully@berkeley.edu>
2022-11-11 21:33:54 -0800

    Initial commit

---
+++ main.c
@@ -0,0 +1,9 @@
+/**
+ * USE AFTER FREE() -> Heap Exploitation ... Why you should keep track of your pointers.
+ * A flaw in glibc's heap management.
+ * Teo Honda-Scully | 2022. 
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
